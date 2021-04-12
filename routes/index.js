70edc44168dfec7f7cccffcb85bb55a473eeb7f1
samuelmayna <samuelmayna@gmail.com>
2021-04-12 15:43:07 +0300

    Protected all the vet details routes

--- routes/index.js
+++ routes/index.js
@@ -1,2 +1,15 @@
+const express = require('express');
+const passport = require('passport');
+
+const { vetSetup } = require('../authMiddleware');
+const vet = require('./vet');
+
+vetSetup(passport);
+
+const router = express.Router();
+
+const { ensureVetIsAuth } = require('../authMiddleware');
+
 exports.auth = require('./auth');
-exports.vet = require('./vet');
+
+exports.vet = router.use(ensureVetIsAuth, vet);
