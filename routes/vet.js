4ef5b2898017ee5d45b352164dc37ad475fa0447
samuelmayna <samuelmayna@gmail.com>
2021-04-12 15:41:31 +0300

    protected all the vet details routes

--- routes/vet.js
+++ routes/vet.js
@@ -1,7 +1,11 @@
 const express = require('express');
+
 const router = express.Router();
 const { vet } = require('../controllers');
 
-router.post('/add-details', vet.postAddDetails);
+const { postAddDetails, postEditDetails } = vet;
+
+router.post('/add-details', postAddDetails);
+router.post('/edit-details', postEditDetails);
 
 module.exports = router;
