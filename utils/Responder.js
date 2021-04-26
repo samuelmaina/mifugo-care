57d47b2c4295c01d59849b6cf7a653430fef5fcc
samuelmayna <samuelmayna@gmail.com>
2021-04-26 10:34:48 +0300

    added a folder to contain the util functions

---
+++ utils/Responder.js
@@ -0,0 +1,42 @@
+class Responder {
+	constructor(res) {
+		this.res = res;
+	}
+	withStatusCode(status) {
+		this.status = status;
+		return this;
+	}
+	withMessage(msg) {
+		this.msg = msg;
+		return this;
+	}
+	withError(err) {
+		this.err = err;
+		return this;
+	}
+	withData(data) {
+		this.data = data;
+		return this;
+	}
+	send() {
+		const { msg, err, data, res, status } = this;
+		rejectIfIsNullOrUndefined(res, 'res');
+		rejectIfIsNullOrUndefined(status, 'status');
+		const body = {};
+		body.message = msg || null;
+		body.error = err || null;
+		for (const prop in data) {
+			if (data.hasOwnProperty.call(data, prop)) {
+				body[prop] = data[prop];
+			}
+		}
+		return res.status(status).json(body);
+	}
+}
+const rejectIfIsNullOrUndefined = (value, field) => {
+	if (typeof value === 'undefined' || value === null) {
+		throw new Error(`${field} must be there.`);
+	}
+};
+
+module.exports = Responder;
---
+++ utils/index.js
@@ -0,0 +1,1 @@
+exports.Responder = require('./Responder');
