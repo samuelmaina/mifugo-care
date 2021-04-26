a4403dd9ea22f0d3d4fe8333aa2594b23beae5fb
samuelmayna <samuelmayna@gmail.com>
2021-04-26 10:32:44 +0300

    added the logic for client authentication

--- routes/index.js
+++ routes/index.js
@@ -1,15 +1,18 @@
 const express = require('express');
 const passport = require('passport');
 
-const { vetSetup } = require('../authMiddleware');
+const { vetSetup, clientSetup } = require('../authMiddleware');
 const vet = require('./vet');
+const client = require('./client');
 
 vetSetup(passport);
+clientSetup(passport);
 
 const router = express.Router();
 
-const { ensureVetIsAuth } = require('../authMiddleware');
+const { ensureVetIsAuth, ensureClientIsAuth } = require('../authMiddleware');
 
 exports.auth = require('./auth');
 
 exports.vet = router.use(ensureVetIsAuth, vet);
+exports.client = router.use(ensureClientIsAuth, client);
