3a446d77408397243ad26003ac23bb3f97365a06
samuelmayna <samuelmayna@gmail.com>
2021-04-26 10:38:30 +0300

    folder that will contain all the logic of tehe application

---
+++ services/auth.js
@@ -0,0 +1,11 @@
+const jwt = require('jsonwebtoken');
+const expiryTimeInSeconds = 3600;
+
+const { SESSION_SECRET } = require('../config/env');
+
+exports.loginIn = (payload, cb) => {
+	const config = {
+		expiresIn: expiryTimeInSeconds,
+	};
+	jwt.sign(payload, SESSION_SECRET, config, cb);
+};
