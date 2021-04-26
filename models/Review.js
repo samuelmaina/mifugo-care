adaa8db884ed656563ed723e7c3f9a10ffc8166c
samuelmayna <samuelmayna@gmail.com>
2021-04-26 10:17:54 +0300

    made each job to hold just one review

--- models/Review.js
+++ models/Review.js
@@ -1,48 +1,30 @@
 const mongoose = require('mongoose');
-const { Reviews } = require('.');
 const { Schema, Types } = mongoose;
 
 const ObjectId = Types.ObjectId;
 
 const Review = new Schema({
-	vet_id: {
+	job_id: {
 		type: ObjectId,
 		required: true,
-		ref: 'Vet',
+		ref: 'Job',
 	},
-	reviews: [
-		{
-			job_id: {
-				type: ObjectId,
-				required: true,
-				ref: 'Vet',
-			},
-			rating: {
-				type: Number,
-				required: true,
-			},
-			review: {
-				type: Object,
-				required: true,
-			},
-		},
-	],
+	rating: {
+		type: Number,
+		required: true,
+	},
+	comment: {
+		type: String,
+		required: true,
+	},
 });
 
 const { statics, methods } = Review;
-statics.creatOne = async function (data) {
+statics.createOne = async function (data) {
 	return await this.create(data);
 };
-statics.findReviewsForJobId = async function (job_id) {
-	return await this.find({ job_id });
-};
-methods.addRating = async function (rating) {
-	this.rating = rating;
-	return await this.save();
-};
-methods.addReview = async function (review) {
-	this.reviews.push(review);
-	return this.save();
+statics.findReviewByJobId = async function (job_id) {
+	return await this.findOne({ job_id });
 };
 
 module.exports = mongoose.model('Review', Review);
