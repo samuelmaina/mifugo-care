a3754a07105be5af51d38b4746b0f54a0befa9eb
samuelmayna <samuelmayna@gmail.com>
2021-07-19 11:49:33 +0300

    Resolved some module require errors

--- models/JobPull.js
+++ models/JobPull.js
@@ -4,49 +4,49 @@ const { Schema, Types } = mongoose;
 const ObjectId = Types.ObjectId;
 
 const JobPull = new Schema({
-  vet_id: {
-    type: ObjectId,
-    required: true,
-    unique: true,
-  },
-  job_ids: [
-    {
-      type: ObjectId,
-      ref: 'Job',
-    },
-  ],
+	vet_id: {
+		type: ObjectId,
+		required: true,
+		unique: true,
+	},
+	job_ids: [
+		{
+			type: ObjectId,
+			ref: 'Job',
+		},
+	],
 });
 const { statics, methods } = JobPull;
 
 statics.createOne = async function (data) {
-  return await this.create(data);
+	return await this.create(data);
 };
 statics.findOneForVetId = async function (vet_id) {
-  return await this.findOne({ vet_id }).populate(
-    'job_ids',
-    'imageUrls description speciality location time'
-  );
+	return await this.findOne({ vet_id }).populate(
+		'job_ids',
+		'imageUrls description speciality location time'
+	);
 };
 
 methods.addJobIdToPull = async function (id) {
-  this.job_ids.push(id);
-  return await this.save();
+	this.job_ids.push(id);
+	return await this.save();
 };
 methods.pupulateJobDetails = async function () {
-  return await this.populate(
-    'job_ids',
-    'imageUrls description speciality location time'
-  ).execPopulate();
+	return await this.populate(
+		'job_ids',
+		'imageUrls description speciality location time'
+	).execPopulate();
 };
 
 methods.removeJobIdFromPull = async function (id) {
-  const jobs = this.job_ids;
+	const jobs = this.job_ids;
 
-  const updatedJobs = jobs.filter((job) => {
-    return job._id.toString() !== id.toString();
-  });
-  this.job_ids = updatedJobs;
-  return await this.save();
+	const updatedJobs = jobs.filter(job => {
+		return job._id.toString() !== id.toString();
+	});
+	this.job_ids = updatedJobs;
+	return await this.save();
 };
 
 module.exports = mongoose.model('JobPull', JobPull);
--- models/index.js
+++ models/index.js
@@ -2,6 +2,6 @@ exports.Client = require('./ClientAuth');
 exports.Vet = require('./VetAuth');
 exports.VetDetails = require('./VetDetails');
 exports.Job = require('./Job');
-exports.JobPull = require('./jobPull');
+exports.JobPull = require('./JobPull');
 exports.Review = require('./Review');
 exports.RecommendedJob = require('./RecommendedJobs');
