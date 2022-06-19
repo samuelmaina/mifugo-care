9145794bd26f43ba9c5ac8023f968b89ac036d5b
samuelmayna <samuelmayna@gmail.com>
2022-06-19 15:29:56 +0300

    Added more info on the home page

--- client/src/views/public/homePage.js
+++ client/src/views/public/homePage.js
@@ -38,11 +38,8 @@ const Header = (
       </div>
       <nav>
         <ul>
-          <li class="current">
-            <a href="/home">Home</a>
-          </li>
           <li>
-            <a href={paths.signUp}>Any Sign Up</a>
+            <a href={paths.signUp}>Join Platform</a>
           </li>
           <li>
             <a href={paths.rootLogin}>Client Login</a>
@@ -58,12 +55,24 @@ const Header = (
 const ShowCase = (
   <section id="showcase">
     <div class="container">
-      <h1>The Site where diseases and management are totally managed.</h1>
+      <h1>MifugoCare, the world's best vet network</h1>
       <p>
-        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Laborum,
-        excepturi deleniti. Ad optio dolor ducimus ipsam consequuntur, tempore
-        unde quam corrupti numquam amet ullam consequatur vitae quia
-        reprehenderit quod beatae.
+        The perks of having pets and domestic animals are beyond measure.
+        Typically, Pets are a great source of companionship, happiness, and
+        wellness. Additionally, these pets are grateful, loyal, and true in
+        their affections. Accordingly, the Kenyan Pet Products Association
+        reveals in its survey that 27% of Kenyan households or approximately 7
+        million families have pets in their homes in a 2020-2021 National Pet
+        Owners Survey. Consequently, different articles indicate that pets are
+        wonderful means to alleviate stress, anxiety, improve immunity, helps
+        fight depression, and have many more benefits.
+      </p>
+      <p> Domestic Animals are also a good source of milk, meat and income. </p>
+      <p>
+        Therefore, animals, whether domestic or wild, need care and attention.
+        Thanks to the existence of veterinarians who diagnose, treat, and
+        research medical conditions of pets’ diseases, livestock, or other
+        animals
       </p>
     </div>
   </section>
@@ -72,7 +81,7 @@ const ShowCase = (
 const NewsLetter = (
   <section id="newsletter">
     <div class="container">
-      <h1>subcribe to our newsletter</h1>
+      <h1>Subcribe to our newsletter</h1>
       <form action="">
         <input type="email" placeholder="Enter your email address" />
         <input type="submit" class="button_1" value="Subscribe" />
@@ -86,42 +95,30 @@ const Services = (
     <div class="container">
       <div class="box">
         <img src="./img/Ann_Bahati_Gals 20170517_193140.jpg" alt="" />
-        <h3> Client,Post Your ill animal</h3>
-        <p>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Sed hic quis
-          esse repudiandae possimus suscipit veniam cumque autem culpa sequi
-          voluptas ratione, temporibus mollitia dicta animi cum magnam molestiae
-          soluta!
-        </p>
+        <h3>
+          Client,Post Your ill animal And get it treated. Give your review
+          according to serviced offered.
+        </h3>
       </div>
       <div class="box">
         <img src="./img/‪+254 703 956617‬ 20170518_215438.jpg" alt="" />
-        <h3>Vet, Manage your treatement schedule</h3>
-        <p>
-          {" "}
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Sed hic quis
-          esse repudiandae possimus suscipit veniam cumque autem culpa sequi
-          voluptas ratione, temporibus mollitia dicta animi cum magnam molestiae
-          soluta!
-        </p>
+        <h3>
+          Vet, Manage your treatement schedule. Get jobs at the confort of your
+          home. Leverage your reputation to get the best jobs{" "}
+        </h3>
       </div>
       <div class="box">
         <img src="./img/‪+254 703 956617‬ 20171114_000425.jpg" alt="" />
-        <h3>Client, be able to rate and write reviews for a vet</h3>
-        <p>
-          {" "}
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Sed hic quis
-          esse repudiandae possimus suscipit veniam cumque autem culpa sequi
-          voluptas ratione, temporibus mollitia dicta animi cum magnam molestiae
-          soluta!
-        </p>
+        <h3>
+          Experience fast, flowless service provision on MifugoCare platform.
+        </h3>
       </div>
     </div>
   </section>
 );
 const Footer = (
   <footer>
-    <p>Samuel Maina &copy; 2021</p>
+    <p>MifugoCare Ltd &copy; 2021</p>
   </footer>
 );
 export class HomePage extends Component {
