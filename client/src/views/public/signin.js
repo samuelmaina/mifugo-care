2e536573abc7011cad6006d4fe0ece1327756307
samuelmayna <samuelmayna@gmail.com>
2022-06-19 14:22:52 +0300

    Removed the different sign up from the page. Only one sign-up will be there

--- client/src/views/public/signin.js
+++ client/src/views/public/signin.js
@@ -1,125 +1,117 @@
-import React, { useState } from 'react';
-import { InputField } from '../../components';
-import { useAuthDispatch, useAuthState, UploadData } from '../../context';
-import { Applogin } from '../../components';
-import * as utils from '../../utils';
-import '../styling/css/interface.css';
+import React, { useState } from "react";
+import { InputField } from "../../components";
+import { useAuthDispatch, useAuthState, UploadData } from "../../context";
+import { Applogin } from "../../components";
+import * as utils from "../../utils";
+import "../styling/css/interface.css";
 
-export const Login = props => {
-	const dispatch = useAuthDispatch();
+export const Login = (props) => {
+  const dispatch = useAuthDispatch();
 
-	const { loading, pass } = useAuthState();
+  const { loading, pass } = useAuthState();
 
-	async function PostLogin(e) {
-		e.preventDefault();
-		let payload = { email, password };
+  let pathname = props.location.pathname.toLocaleLowerCase();
+  pathname =
+    pathname === "/auth/log-in/client"
+      ? "/auth/log-in/client"
+      : "/auth/log-in/vet";
 
-		let pathname = props.location.pathname.toLocaleLowerCase();
-		pathname =
-			pathname === '/auth/log-in/client'
-				? '/auth/log-in/client'
-				: '/auth/log-in/vet';
+  async function PostLogin(e) {
+    e.preventDefault();
+    let payload = { email, password };
 
-		let validation_info = utils.AssertAndValidateLogin(payload);
+    let validation_info = utils.AssertAndValidateLogin(payload);
 
-		if (validation_info === 'valid') {
-			let response = await UploadData(dispatch, payload, pathname);
+    if (validation_info === "valid") {
+      let response = await UploadData(dispatch, payload, pathname);
 
-			if (response.vp_P_A === pass.vet) {
-				utils.clearContextErrors(dispatch);
-				props.history.push('/vet/homepage');
-			} else if (response.vp_P_A === pass.client) {
-				utils.clearContextErrors(dispatch);
-				props.history.push('/client/homepage');
-			} else if (response.vp_P_A === pass.vetUpdate) {
-				utils.clearContextErrors(dispatch);
-				props.history.push('/vet/completeRegistration');
-			}
-		} else {
-			dispatch({ type: 'APIACCESS_ERROR', error: validation_info });
-		}
-	}
+      if (response.vp_P_A === pass.vet) {
+        utils.clearContextErrors(dispatch);
+        props.history.push("/vet/homepage");
+      } else if (response.vp_P_A === pass.client) {
+        utils.clearContextErrors(dispatch);
+        props.history.push("/client/homepage");
+      } else if (response.vp_P_A === pass.vetUpdate) {
+        utils.clearContextErrors(dispatch);
+        props.history.push("/vet/completeRegistration");
+      }
+    } else {
+      dispatch({ type: "APIACCESS_ERROR", error: validation_info });
+    }
+  }
 
-	const handleurlChange = (e, group) => {
-		localStorage.setItem('group', group);
-	};
+  const handleurlChange = () => {
+    pathname === "/auth/log-in/client"
+      ? localStorage.setItem("group", "client")
+      : localStorage.setItem("group", "vet");
+  };
 
-	const [email, setEmail] = useState('');
-	const [password, setPassword] = useState('');
-	const [checked, handleToggle] = useState(false);
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [checked, handleToggle] = useState(false);
 
-	const signUpRoute = '/auth/sign-up';
+  const signUpRoute = "/auth/sign-up";
 
-	return (
-		<React.Fragment>
-			<Applogin className="app">
-				<form>
-					<div>
-						<InputField
-							width={'100%'}
-							type={'email'}
-							name={'email'}
-							placeholder={'Email'}
-							setField={setEmail}
-						/>
-						<InputField
-							width={'100%'}
-							type={'password'}
-							name={'password'}
-							placeholder={'password'}
-							setField={setPassword}
-						/>
-					</div>
-					<section>
-						<input
-							type="checkBox"
-							name="checkbox"
-							onChange={e => {
-								handleToggle(!checked);
-							}}
-						/>
-						<span>Remember me</span>
-						<span className="floated_to_right">
-							<a href="\">Forgot password</a>
-						</span>
-						<br />
-					</section>
-					<button
-						data-testid="submit-button"
-						style={{
-							marginTop: '50px',
-							borderRadius: '50px',
-							width: '90%',
-							marginLeft: '4%',
-						}}
-						onClick={e => PostLogin(e)}
-					>
-						Login {loading ? '...' : null}
-					</button>
-					<p>
-						No account? Join VetHealth as a{' '}
-						<a
-							href={signUpRoute}
-							onClick={e => {
-								handleurlChange(e, 'vet');
-							}}
-						>
-							vet Officer
-						</a>{' '}
-						or
-						<a
-							href={signUpRoute}
-							onClick={e => {
-								handleurlChange(e, 'client');
-							}}
-						>
-							{' '}
-							Client
-						</a>
-					</p>
-					<utils.ViewErrorMessage />
-				</form>
-			</Applogin>
-		</React.Fragment>
-	);
+  return (
+    <React.Fragment>
+      <Applogin className="app">
+        <form>
+          <div>
+            <InputField
+              width={"100%"}
+              type={"email"}
+              name={"email"}
+              placeholder={"Email"}
+              setField={setEmail}
+            />
+            <InputField
+              width={"100%"}
+              type={"password"}
+              name={"password"}
+              placeholder={"password"}
+              setField={setPassword}
+            />
+          </div>
+          <section>
+            <input
+              type="checkBox"
+              name="checkbox"
+              onChange={(e) => {
+                handleToggle(!checked);
+              }}
+            />
+            <span>Remember me</span>
+            <span className="floated_to_right">
+              <a href="\">Forgot password</a>
+            </span>
+            <br />
+          </section>
+          <button
+            data-testid="submit-button"
+            style={{
+              marginTop: "50px",
+              borderRadius: "50px",
+              width: "90%",
+              marginLeft: "4%",
+            }}
+            onClick={(e) => PostLogin(e)}
+          >
+            Login {loading ? "..." : null}
+          </button>
+          <p>
+            No account?
+            <a
+              href={signUpRoute}
+              onClick={(e) => {
+                handleurlChange();
+              }}
+            >
+              Sign UP
+            </a>
+          </p>
+          <utils.ViewErrorMessage />
+        </form>
+      </Applogin>
+    </React.Fragment>
+  );
 };
